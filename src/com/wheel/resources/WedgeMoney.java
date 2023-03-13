6c540ad17e3fef81191654fbca9a1ffdc7f49b15
Chad Kellum <chad.kellum@tlgcohort.com>
2023-03-13 11:41:37 -0700

    Chad - filled in Player Get/Set methods, constructors, player number function, added more wedges to the WedgeMoney enum. Tried added instances of the money wedges on the wheel.

--- src/com/wheel/resources/WedgeMoney.java
+++ src/com/wheel/resources/WedgeMoney.java
@@ -1,6 +1,6 @@
 package com.wheel.resources;
 
-enum WedgeMoney implements Wedge{
-    $500, $600, $700, $800, $900
+public enum WedgeMoney implements Wedge{
+    US_500, US_550, US_600, US_650, US_700, US_750, US_800, US_850, US_900
 
 }
