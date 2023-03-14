257bc403262fd46deccd9e2b0e715df502b19082
Chad Kellum <chad.kellum@tlgcohort.com>
2023-03-14 13:34:19 -0700

    Added Stringbuilder to Host to offer choices based on how much money a player has. No money, no vowels. Added paranthesis to the deductMoney method in player class

--- src/com/wheel/sys/Player.java
+++ src/com/wheel/sys/Player.java
@@ -83,6 +83,6 @@ public class Player {
     }
 
     public int deductMoney(int vowelCost) {
-        return getRoundBalance() - vowelCost;
+        return (getRoundBalance() - vowelCost);
     }
 }
